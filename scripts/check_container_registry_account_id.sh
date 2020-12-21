be574ef9045b29ba81b7375c8dc504293c184245
cait <caitlin@madetech.com>
2020-12-21 11:04:08 +0000

    Fix spelling mistake in error message

--- scripts/check_container_registry_account_id.sh
+++ scripts/check_container_registry_account_id.sh
@@ -3,6 +3,6 @@
 set -euo pipefail
 
 if [ -z ${SHARED_SERVICES_ACCOUNT_ID} ]; then
-  echo "Please set enviroment variable SHARED_SERVICES_ACCOUNT_ID for shared services";
+  echo "Please set environment variable SHARED_SERVICES_ACCOUNT_ID for shared services";
   exit 1;
 fi
