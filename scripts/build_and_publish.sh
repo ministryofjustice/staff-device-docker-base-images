4107a8ddb85ce812a967c0dce0db2f5fa71ccb94
Joshua-Luke Bevan <63861228+jbevan4@users.noreply.github.com>
2021-01-13 15:35:39 +0000

    change the way in which we tag and publish base images (#6)
    * change the way in which we tag and publish base images

--- scripts/build_and_publish.sh
+++ scripts/build_and_publish.sh
@@ -6,8 +6,11 @@ cd ./images
 
 for image_name in * ; do
   docker build -t $image_name ./$image_name
+  tag=$( < $image_name/Dockerfile cut -f2 -d ' ' | sed 's/[^a-zA-Z0-9]/-/g' )
   repository_url=${SHARED_SERVICES_ACCOUNT_ID}.dkr.ecr.eu-west-2.amazonaws.com/$image_name
   aws ecr get-login-password | docker login --username AWS --password-stdin ${repository_url}
-  docker tag $image_name:latest ${repository_url}:latest
-  docker push ${repository_url}:latest  
+  docker tag $image_name ${repository_url}:$tag
+  docker push ${repository_url}:$tag  
+  docker tag $image_name ${repository_url}:latest
+  docker push ${repository_url}:latest
 done
