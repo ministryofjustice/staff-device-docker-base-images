fe1378cf0fcf4021d18419b308d7775cbfa1d44d
Touhidul Islam <touhidul.islam@justice.gov.uk>
2021-03-25 12:20:29 +0000

    creates a script to extract version information from all Dockerfiles script is designed to export a prometheus compatible metric file in a separate folder.

---
+++ scripts/extract_image_information.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+echo "" > prometheus_metrics/docker_image_versions.txt
+
+for d in images/*/; do
+
+info=$(cat $d/Dockerfile | grep FROM | cut -d M -f2)
+
+imagesName=$(echo $info | cut -d : -f1)
+imageVersion=$(echo $info | cut -d : -f2)
+
+echo "docker_image_versions{imageName=\"${imagesName}\",imageVersion=\"${imageVersion}\"}" >> prometheus_metrics/docker_image_versions.txt
+
+done
+
+cat prometheus_metrics/docker_image_versions.txt
